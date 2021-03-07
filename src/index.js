b4367f61b51325d0bdb43cc61db008690af7fe37
dm25an22 <dm25an22@gmail.com>
2021-03-07 17:23:14 +0100

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = bracketsConfig.map(it => it.join(""));
+  const arr = [...str];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let k = 0; k < config.length; k++) {
+      if (`${arr[i]}${arr[i + 1]}` === config[k]) {
+         arr.splice(i, 2);
+        i = -1;
+      }
+    }
+  }
+  
+  return arr.length === 0;
 }
